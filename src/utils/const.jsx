13943beace528e275fef99ca9039bd3ee9cadbf8
TranSon <tson113355@gmail.com>
2024-09-20 22:07:27 +0700

    feat: Update Discord contact link in const.jsx

--- src/utils/const.jsx
+++ src/utils/const.jsx
@@ -1,7 +1,7 @@
 export const USD_VND = 24930;
 export const ZALO = 'https://zalo.me/0338062161'
 export const WHATSAPP = 'https://wa.me/qr/UGGGPXH3765DM1'
-export const DISCORD = 'https://discord.gg/hgcVuRHA'
+export const DISCORD = 'https://discord.gg/JqZtZ8b7Mk'
 export const FACEBOOK = 'https://www.facebook.com/profile.php?id=61565808101914&mibextid=LQQJ4d'
 export const PAYPAL = 'Paypalmina91@gmail.com'
 export const MOMO = '0338062161'
